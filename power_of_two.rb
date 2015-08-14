4c02b979ceedd46ecd04afbcb2512390ae68c234
hilpitome <hilpitome@gmail.com>
2015-08-14 10:46:22 +0300

    completed power_of_two

--- power_of_two.rb
+++ power_of_two.rb
@@ -8,17 +8,19 @@
 # Difficulty: medium.
 
 def is_power_of_two?(num)
-	if num == 1
-		return true
-	
-	elsif num != 0 && num % 2 == 0
-		return true
-
-	else
-		return false
+	while num >= 1 
+		if num == 1 
+			return true
+		elsif num % 2 == 0
+		num = num/2
+	    elsif num % 2 != 0
+		return false  
+		
+	    end
 
 	end
-			
+
+	return false		
 end
 
 # These are tests to check that your code is working. After writing
@@ -27,5 +29,5 @@ end
 puts('is_power_of_two?(1) == true: ' + (is_power_of_two?(1) == true).to_s)
 puts('is_power_of_two?(16) == true: ' + (is_power_of_two?(16) == true).to_s)
 puts('is_power_of_two?(64) == true: ' + (is_power_of_two?(64) == true).to_s)
-puts('is_power_of_two?(78) == false: ' + (is_power_of_two?(78) == false).to_s)
+puts('is_power_of_two?(79) == false: ' + (is_power_of_two?(78) == false).to_s)
 puts('is_power_of_two?(0) == false: ' + (is_power_of_two?(0) == false).to_s)
