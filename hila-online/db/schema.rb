2a33d1aad469668ebd9862c65246d3a3b84abf64
hilpitome <hilpitome@gmail.com>
2015-09-16 10:07:10 +0300

    added company field to db

--- hila-online/db/schema.rb
+++ hila-online/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916063112) do
+ActiveRecord::Schema.define(version: 20150916070539) do
 
   create_table "deals", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150916063112) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "company"
   end
 
 end
